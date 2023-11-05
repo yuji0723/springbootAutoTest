f1702b16dff16e8335025c4201c4f76f33ef3556
yuji0723 <631160360@qq.com>
2023-11-05 15:29:49 +0800

    TEST-00 fix UserMapper

--- src/main/java/com/jin/AutoApplication.java
+++ src/main/java/com/jin/AutoApplication.java
@@ -4,9 +4,11 @@ package com.jin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.jin.mapper")
+//@MapperScan("com.jin.mapper")
+@ComponentScan(basePackages = {"com.jin"})
 public class AutoApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutoApplication.class, args);
--- src/main/java/com/jin/mapper/UserMapper.java
+++ src/main/java/com/jin/mapper/UserMapper.java
@@ -2,15 +2,18 @@ package com.jin.mapper;
 
 import com.jin.entity.UserEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author 大力pig
  * @since 2023-11-02
  */
+
+@Mapper
 public interface UserMapper extends BaseMapper<UserEntity> {
 
 }
